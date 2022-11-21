c0ebbe5d65585f06d1c699c5472b5b64fc759845
laodeshaldanfalih <77shaldan77@gmail.com>
2022-11-21 15:30:34 +0700

    menambahkan models dan screens file

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/models/models.dart';
+
+class ProductsOverviewScreen extends StatelessWidget {
+  // final List<Product> loadesProduct = {}
+
+  @override
+  Widget build(BuildContext recontext) {
+    return Scaffold();
+  }
+}
